ed731a18249861d065fef3d28247d0dc48f0ac09
Kevin Frei <freik@fb.com>
2018-01-22 07:04:48 -0800

    A little refactoring this morning. Hurray for the Makefile!

---
+++ dbgcfg.h
@@ -0,0 +1,29 @@
+#if !defined(DBGCFG_H)
+#define DBGCFG_H
+
+// #define DEBUG 1
+
+#if DEBUG
+#define DBG(a) a
+
+void dumpVal(unsigned long v, const char* header = nullptr) {
+  if (header)
+    Serial.print(header);
+  Serial.println(v);
+}
+void dumpHex(unsigned long v, const char* header = nullptr) {
+  if (header)
+    Serial.print(header);
+  Serial.println(v, HEX);
+}
+#if DEBUG == 2
+#define DBG2(a) a
+#else
+#define DBG2(a)
+#endif
+#else
+#define DBG(a)
+#define DBG2(a)
+#endif
+
+#endif
